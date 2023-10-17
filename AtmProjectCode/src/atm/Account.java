2d9c96f7e45ad1b4abc7a9f6ac249c6a36d308e1
Gur <guermoud.@gmail.com>
2023-10-17 23:31:38 +0100

    added a login method in Account class and test it in my app class

--- AtmProjectCode/src/atm/Account.java
+++ AtmProjectCode/src/atm/Account.java
@@ -1,13 +1,37 @@
 package atm;
+import java.sql.*;
 
 public class Account {
 	private String dateCreation;
 	private int balance;
 	private String accountType;
-	private int accountNumber;
+	private String accountNumber;
+	private String password;
+	private PreparedStatement p = null;
+	private ResultSet rs = null;
+	private String query = "SELECT * FROM CLIENTS";
+	private Connection conn = ConnectDB.getConn();
 	
-	
-	public void Login(bankCard cardNumber, String password) {
+	public Account(String accountNumber, String password) {
+		this.accountNumber = accountNumber;
+		this.password = password;
+	}
+	public void Login() {
+		try {
+			p = conn.prepareStatement(query);
+			rs = p.executeQuery();
+			while(rs.next()) {
+				if((rs.getString(4)).equals(accountNumber) && (rs.getString(5).equals(password))) {
+					System.out.println("Welcome");	
+			}
+		 }
+		}
+		catch(SQLException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
 		
 	}
   
--- AtmProjectCode/src/atm/App.java
+++ AtmProjectCode/src/atm/App.java
@@ -1,10 +1,12 @@
 package atm;
-import java.sql.*;
+
 
 public class App {
 
 	public static void main(String[] args) {
-		ConnectDB.getConn();
+		//ConnectDB.getConn();
+		Account a = new Account("1111222233334444","secure456");
+		a.Login();
 		
 
 	}
