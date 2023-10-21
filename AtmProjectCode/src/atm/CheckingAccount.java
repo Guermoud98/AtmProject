8a6e4c56b5dcce250c03bd6ce6a88dd6e3c3e69c
Gur <guermoud.@gmail.com>
2023-10-21 18:52:48 +0100

    Create the CheckingAccount and SavingAccount classes.

--- AtmProjectCode/src/atm/CheckingAccount.java
+++ AtmProjectCode/src/atm/CheckingAccount.java
@@ -1,5 +1,28 @@
 package atm;
 
-public class CheckingAccount {
+import java.sql.SQLException;
+
+public class CheckingAccount extends Account {
+	private float overDratFee;
+
+	public CheckingAccount(String cardNumber, float overDratFee) throws SQLException {
+		super(cardNumber);
+		this.overDratFee = overDratFee;
+	}
+
+	public float getOverDratFee() {
+		return overDratFee;
+	}
+
+	public void setOverDratFee(float overDratFee) {
+		this.overDratFee = overDratFee;
+	}
+
+	@Override
+	public String toString() {
+		return "CheckingAccount [overDratFee=" + overDratFee + "]";
+	}
+	
+	
 
 }
--- AtmProjectCode/src/atm/SavingAccount.java
+++ AtmProjectCode/src/atm/SavingAccount.java
@@ -1,5 +1,27 @@
 package atm;
 
-public class SavingAccount {
+import java.sql.SQLException;
+
+public class SavingAccount extends Account{
+	private float interestFee;
+	
+	public SavingAccount(float interestFee, String cardNumber) throws SQLException {
+		super(cardNumber);
+		this.interestFee = interestFee;
+	}
+
+	public float getInterestFee() {
+		return interestFee;
+	}
+
+	public void setInterestFee(float interestFee) {
+		this.interestFee = interestFee;
+	}
+
+	@Override
+	public String toString() {
+		return "SavingAccount [interestFee=" + interestFee + "]";
+	}
+	
 
 }
